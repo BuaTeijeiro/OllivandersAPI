275ff2ac1f9184e2be7762c136cc9cf35c0daa67
BuaTeijeiro <bua.teijeiro.david@gmail.com>
2024-05-21 16:24:40 +0200

    Test(ItemFactoryTest): Añadir test de caso null y conexion a base de datos

--- src/test/java/edu/badpals/domain/ItemFactoryTest.java
+++ src/test/java/edu/badpals/domain/ItemFactoryTest.java
@@ -1,9 +1,13 @@
 package edu.badpals.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -59,5 +63,30 @@ class ItemFactoryTest {
         normal.updateItem();
         assertEquals(3, normal.getSellIn());
         assertEquals(80, normal.getQuality());
+    }
+
+    @Test
+    void createInvalidType(){
+        Item item = new Item("sulfuras", 3, 80, "SULFURA");
+        Updateable normal = ItemFactory.makeItem(item);
+        assertNull(normal);
+    }
+
+    @Test
+    void conexionBaseDeDatos(){
+        List<Item> items = Item.listAll();
+        List<Updateable> updateables = new ArrayList<>();
+        for (Item item: items){
+            updateables.add(ItemFactory.makeItem(item));
+        }
+        assertTrue(updateables.get(0) instanceof NormalItem);
+        assertTrue(updateables.get(1) instanceof AgedBrie);
+        assertTrue(updateables.get(2) instanceof NormalItem);
+        assertTrue(updateables.get(3) instanceof Sulfuras);
+        assertTrue(updateables.get(4) instanceof Sulfuras);
+        assertTrue(updateables.get(5) instanceof BackstagePass);
+        assertTrue(updateables.get(6) instanceof BackstagePass);
+        assertTrue(updateables.get(7) instanceof BackstagePass);
+        assertTrue(updateables.get(8) instanceof Conjured);
     }
 }
