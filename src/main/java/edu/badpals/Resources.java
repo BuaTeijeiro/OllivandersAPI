c03d57676cf92d499af3e8f7c847cadcf74ead2a
davidbua <bua.teijeiro.david@gmail.com>
2024-05-23 11:20:08 +0200

    feat(Resources): Añadir gets de ver Stock y ver stock por nombre

--- src/main/java/edu/badpals/Resources.java
+++ src/main/java/edu/badpals/Resources.java
@@ -4,6 +4,13 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+
+import edu.badpals.domain.Item;
 
 @Path("/do")
 public class Resources {
@@ -16,6 +23,22 @@ public class Resources {
     public String updateStock(){
         service.updateDatabase();
         return "Accion llevada a cabo correctamente";
+    }
+
+    @GET
+    @Path("/seeStock")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getStock(){
+        List<Item> items = Item.getAllItems();
+        return Response.status(Response.Status.CREATED).entity(items).build();
+    }
+
+    @GET
+    @Path("/see/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response showItemsByName(@PathParam("name") String name){
+        List<Item> items = Item.getItemsByName(name);
+        return Response.status(Response.Status.CREATED).entity(items).build();
     }
 
 }
